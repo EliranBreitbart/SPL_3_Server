bfd47d46691bf8562af2da989bc77bd28e301aa9
Eliran Breitbart <eliranbr@post.bgu.ac.il>
2022-01-04 14:10:41 +0200

    04/01/2022
    fixes to protocol

--- src/main/java/net/srv/TPCMain.java
+++ src/main/java/net/srv/TPCMain.java
@@ -1,10 +1,7 @@
 package net.srv;
 
-import net.api.BidiMessagingProtocol;
 import net.api.bidi.BidiProtocol;
 import net.api.encdec;
-
-import java.util.function.Supplier;
 
 public class TPCMain {
     public static void main(String[] args){
